2b7f0ed0022d8ff0be8675370daa5e5e842b203b
Mutoi01 <pngando9@gmail.com>
2022-09-18 11:15:06 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+    attr_reader  :title
+    attr_accessor :name, :page_count, :author , :genre
 
-
+    def initialize title
+        @title = title
+    end
+    def turn_page
+        puts 'Flipping the page...wow, you read fast!'
+    end
+end
